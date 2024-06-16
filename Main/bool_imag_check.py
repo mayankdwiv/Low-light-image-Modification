785587c325bdb755ac9bf25beec4c64a8b73ecbd
mayankdwiv <122630722+mayankdwiv@users.noreply.github.com>
2024-06-16 16:35:40 +0530

    Updated bool_imag_check.py

--- Main/bool_imag_check.py
+++ Main/bool_imag_check.py
@@ -9,18 +9,7 @@ from matplotlib import pyplot as plt
 from bm3d import bm3d
 from typing import Union
 def bool_image(file_name: str) -> bool:
-    """Checks if a file is of 'bmp', 'jpg', 'png' or 'tif' format.
-
-    Returns True if a file name ends with any of these formats, and False is 
-    returned otherwise.
-
-    ## Args:
-        file_name (str) : A string representing a file name.
-
-    ## Returns:
-        bool_value (bool) : A boolean value answering if the provided file 
-        name is of the given four formats.   
-    """
+    
 
     bool_value = file_name[-3:] in ['bmp', 'jpg', 'png', 'tif']
 
